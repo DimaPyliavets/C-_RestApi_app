ef4399bdfa58218e0ca9801fc8990c5144c2b8f9
Dmytro Pyliavets <demitriy26081999@gmail.com>
2023-05-16 20:02:53 +0200

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required] // поле обовязкове для заповнення не може бути пустим
+        public string Name { get; set; }
+
+        [Required] // поле обовязкове для заповнення не може бути пустим
+        public int Price { get; set; }
+    }
+}
